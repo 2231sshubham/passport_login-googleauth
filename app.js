44016c2e7a3d7961dac8b39be681d648e0f944e4
2231sshubham <2231sshubham@gmail.com>
2021-11-05 14:37:18 +0530

    completed google-auth

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const LocalStrategy = require('passport-local').Strategy;
 const {sequelize,User} = require('./models')
-const session = require("express-session");
+const cookieSession = require('cookie-session')
 const config = require('./config')
 
 const app = express();
@@ -12,41 +11,45 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(session({secret: "secret"}));
+
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new GoogleStrategy({
-  clientID: config.clientID,
-  clientSecret: config.client_secret,
-  callbackURL: config.redirect_uris,
-  accessType: "offline",
-  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-  },
-  (accessToken, refreshToken, profile, cb) => {
-         cb(null, extractProfile(profile));
-}));
-
 passport.serializeUser((user, cb) => {
-          cb(null, user);
+    cb(null, user);
 });
-passport.deserializeUser((obj, cb) => {
-          cb(null, obj);
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
+passport.use(new GoogleStrategy({
+    clientID: config.clientID,
+    clientSecret: config.client_secret,
+    callbackURL: config.redirect_uris,
+    passReqToCallback: true
+  },
+  function (request, accessToken, refreshToken, profile, cb) {
+    User.create({name:profile.emails[0].value})
+    return cb(null, profile);
+  }
+));
 
+app.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get("/login", passport.authenticate('google', {
-  scope: ['email', 'profile']
-}), (req, res) => {});
-
-
+app.get('/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    res.redirect('/success');
+  }
+);
 
 app.get("/success",(req,res) => {
   res.json({msg:"Google login success"});
 })
-
   
 app.listen(3000,async () => {
   console.log("Server started");
